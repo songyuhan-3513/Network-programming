42ebbb75286e5f02852ec35926ca4792912e16a8
Yuhan Song <775856493@qq.com>
2026-04-11 23:20:35 +0800

    chatroom

---
+++ chatroomclient.cc
@@ -0,0 +1,82 @@
+#define _GNU_SOURCE 1
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <poll.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 64
+
+int main(int argc, char *argv[]) {
+    if(argc <= 2)
+    {
+        printf("usage: %s ip_address port_number\n", basename(argv[0]));
+        return 1;
+    }
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    sockaddr_in server_address;  // IPV4地址结构
+    bzero(&server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &server_address.sin_addr); // IPV4点分十进制转换成整型，网络字节序
+    server_address.sin_port = htons(port);
+
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sockfd >= 0);
+    if(connect(sockfd, (sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        printf("connection failed\n");
+        close(sockfd);
+        return 1;
+    }
+
+    pollfd fds[2];
+    fds[0].fd = 0;
+    fds[0].events = POLLIN;
+    fds[0].revents = 0;
+    fds[1].fd = sockfd;
+    fds[1].events = POLLIN | POLLRDHUP; // 注册对端关闭事件
+    fds[1].revents = 0;
+
+    char read_buf[BUFFER_SIZE];
+    int pipefd[2];
+    int ret = pipe(pipefd); // 创建管道
+    assert(ret != -1);
+
+    while(1)
+    {
+        ret = poll(fds, 2, -1);
+        if(ret < 0)
+        {
+            printf("poll failure\n");
+            break;
+        }
+
+        if(fds[1].revents & POLLRDHUP)
+        {
+            printf("server close the connection");
+            break;
+        }
+        else if(fds[1].revents & POLLIN)
+        {
+            memset(read_buf, '\0', BUFFER_SIZE);
+            recv(fds[1].fd, read_buf, BUFFER_SIZE, 0);
+            printf("%s\n", read_buf);
+        }
+
+        if(fds[0].revents & POLLIN)
+        {
+            ret = splice(0, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE); // 把终端输入的数据写到管道写端
+            ret = splice(pipefd[0], NULL, sockfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE); // 把管道读端的数据写到连接socket
+        }
+    }
+    close(sockfd);
+    return 0;
+}
